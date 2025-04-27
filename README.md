edbf03ca523830508f3f03101a712a47da8ab1b0
SLMM <sergio.marques@puc-campinas.edu.br>
2025-04-27 16:35:35 -0300

    inicializando o projeto

--- README.md
+++ README.md
@@ -6,3 +6,24 @@ Repositorio glitch para API
 https://sincere-magnificent-cobweb.glitch.me/
 
 Repositorio para Flutter A ser disponibilizado.
+
+
+# API Node.js com Express e Swagger
+
+## Como usar:
+
+1. Instale as dependências:
+
+```bash
+npm install
+npm start
+
+
+GET http://localhost:3000/datetime
+
+POST http://localhost:3000/concat com JSON { "value": "texto" }
+
+Documentação Swagger:
+
+http://localhost:3000/api-docs
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const routes = require('./routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Swagger em http://localhost:${port}/api-docs`);
+});
