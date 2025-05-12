84fb86b706d963a1a2a721a294acbe905ebee890
SLMM <sergio.marques@puc-campinas.edu.br>
2025-05-11 23:27:31 -0300

    atualizado test

--- api/tests/index.test.js
+++ api/tests/index.test.js
@@ -1,8 +1,11 @@
  
 //index.test.js
-const index = require('./../controllers/funcoes_base.js')
-test('Aplicar desconto', () => {
-    const result = index.aplicarDesconto(10,5);
-    expect(result).toEqual(5);
+const {aplicarDesconto} = require('./../controllers/funcoes_base.js')
+
+describe('Texte exemplo', () => {
+    test('Aplicar desconto', () => {
+        const result = aplicarDesconto(10,5);
+        expect(result).toEqual(5);
+    })
 })
 
