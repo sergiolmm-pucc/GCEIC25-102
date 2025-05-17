b69eab7ed2611c8d0ca1bf438324c11ace8fd18d
sergiolmm-pucc <104084417+sergiolmm-pucc@users.noreply.github.com>
2025-05-17 00:31:23 -0300

    Update main.dart

--- web/gcseic25/lib/main.dart
+++ web/gcseic25/lib/main.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tela Inicial'),
+        title: Text('Tela Inicial 102'),
       ),
       body: Center(
         child: Column(
