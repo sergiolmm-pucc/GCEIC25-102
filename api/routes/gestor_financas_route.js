e4942590f240e44febd400a9ca76acfc756f1d14
Bernardo Amaro <amarobernardo954@gmail.com>
2025-05-21 22:41:52 -0300

    Rota imprementada

--- api/routes/gestor_financas_route.js
+++ api/routes/gestor_financas_route.js
@@ -1,1 +1,9 @@
-// fazer
+const express = require("express");
+const router = express.Router();
+const {
+  calcularFinancas,
+} = require("../controllers/gestor_financas_controller");
+
+router.post("/calcular-financas", calcularFinancas);
+
+module.exports = router;
