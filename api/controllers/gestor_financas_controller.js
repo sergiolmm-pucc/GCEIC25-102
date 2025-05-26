1cc50bb5849c356382a4d799da6cf840f1d35de4
BernardoAmaro <138726582+BernardoAmaro@users.noreply.github.com>
2025-05-26 20:44:08 -0300

    Refatorando as funções - parte 2

--- api/controllers/gestor_financas_controller.js
+++ api/controllers/gestor_financas_controller.js
@@ -11,3 +11,21 @@ function calcularINSS(salario) {
   if (salario <= 8157.41) return inssFaixa4(salario);
   return inssTeto();
 }
+
+// Funções auxiliares IRPF
+function irpfIsento(salarioBase) { return 0; }
+function irpfFaixa1(salarioBase) { return salarioBase * 0.075 - 169.44 - 607.20; }
+function irpfFaixa2(salarioBase) { return salarioBase * 0.15 - 381.44 - 607.20; }
+function irpfFaixa3(salarioBase) { return salarioBase * 0.225 - 662.77 - 607.20; }
+function irpfFaixa4(salarioBase) {
+  const ir = salarioBase * 0.275 - 896.00 - 607.20;
+  return ir < 0 ? 0 : ir;
+}
+
+function calcularIRPF(salarioBase) {
+  if (salarioBase <= 2259.20) return irpfIsento(salarioBase);
+  if (salarioBase <= 2826.65) return irpfFaixa1(salarioBase);
+  if (salarioBase <= 3751.05) return irpfFaixa2(salarioBase);
+  if (salarioBase <= 4664.68) return irpfFaixa3(salarioBase);
+  return irpfFaixa4(salarioBase);
+}
