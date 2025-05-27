aabd129479a010c474600edee7d84e617349db47
paulobaima <paulo.baima04@gmail.com>
2025-05-26 21:20:53 -0300

    Commit inicial da função de login

---
+++ web/gcseic25/lib/equipes/equipe2/login_screen_equipe2.dart
@@ -0,0 +1,10 @@
+// PROJETO SERGIO NODE/web/gcseic25/lib/equipes/equipe2/login_screen_equipe2.dart
+import 'package:flutter/material.dart';
+import 'package:gcseic25/equipes/equipe2/travel_calculator_screen_equipe2.dart';
+
+class LoginScreenEquipe2 extends StatefulWidget { //
+const LoginScreenEquipe2({Key? key}) : super(key: key);
+
+@override
+State<LoginScreenEquipe2> createState() => _LoginScreenEquipe2State();
+}
