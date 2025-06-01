0065e0bd4bc3fb15f998e8c26961e8682b70e3fb
LucasGiazzi <lucasgiazzi@gmail.com>
2025-06-01 19:44:43 -0300

    rota e frontend estimativa de tempo

---
+++ api/tests/calculoTempoEstimadoE5.test.js
@@ -0,0 +1,24 @@
+const controller = require('../controllers/calculoViagemEquipe5Controller');
+
+test('Deve retornar tempo estimado corretamente', () => {
+    const req = {
+        body: {
+            distancia: 240,
+            velocidadeMedia: 80
+        }
+    };
+
+    const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    };
+
+    controller.estimativaTempo(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+        success: true,
+        tempoEstimadoHoras: 3,
+        tempoFormatado: "3h 0min"
+    });
+});
